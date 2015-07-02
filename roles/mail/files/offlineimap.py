56cb6276583b7d6d0e841a39dca0fbe2da818330
Sam Morrison <sam@cnstechgroup.com>
2015-07-02 18:52:18 +0900

    added offlineimap password script

---
+++ roles/mail/files/offlineimap.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import re, subprocess, os, sys
+
+def mailpasswd(acct):
+  acct = os.path.basename(acct)
+  path = "Mail/%s" % acct
+  args = ["/usr/bin/pass", path]
+  
+  try:
+    return subprocess.check_output(args).strip()
+  except subprocess.CalledProcessError:
+    return ""
+
+def prime_gpg_agent():
+  ret = False
+  i = 1
+  while not ret:
+    ret = (mailpasswd("prime") == "prime")
+    if i > 2:
+      from offlineimap.ui import getglobalui
+      sys.stderr.write("Error reading in passwords. Terminating.\n")
+      getglobalui().terminate()
+    i += 1
+  return ret
+
+prime_gpg_agent()
