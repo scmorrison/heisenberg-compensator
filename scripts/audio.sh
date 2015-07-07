e198a9638bcf8a5c2148dd47848277a760750f48
Sam Morrison <sam@cnstechgroup.com>
2015-07-07 11:10:28 +0900

    cleaned up mail role, added seperate offlineimap presynchooks

--- scripts/audio.sh
+++ scripts/audio.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ansible-playbook -i hosts site.yml --ask-sudo-pass --tags="base,audio"  --vault-password-file=scripts/vaultpass
+#ansible-playbook -i hosts site.yml --ask-sudo-pass --tags="base,audio"  --vault-password-file=scripts/vaultpass
+ansible-playbook -i hosts site.yml --ask-sudo-pass --tags "ardour" --vault-password-file=scripts/vaultpass
--- scripts/mail.sh
+++ scripts/mail.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i hosts site.yml --ask-sudo-pass --tags "oitemplate" --vault-password-file=scripts/vaultpass
+ansible-playbook -i hosts site.yml --ask-sudo-pass --tags "mailfilter,oitemplate" --vault-password-file=scripts/vaultpass
