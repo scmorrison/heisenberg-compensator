04b3974c52e7331a186b8e465be359c185e4731c
Sam Morrison <sam@cnstechgroup.com>
2016-01-06 10:24:31 +0900

    use Mozilla's newer download paths for Firefox

--- scripts/updatefirefox-latest.sh
+++ scripts/updatefirefox-latest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i hosts site.yml --ask-sudo-pass --tags "firefox" -e "release=latest arch=i686" --vault-password-file=scripts/vaultpass
+ansible-playbook -i hosts site.yml --ask-sudo-pass --tags "firefox" -e "release=latest arch=64" --vault-password-file=scripts/vaultpass
